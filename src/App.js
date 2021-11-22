93b4e23947d0fc640b75b43e0447d196384a40b5
RomanIlienko <75617889+RomanIlienko@users.noreply.github.com>
2021-11-22 20:49:11 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
-import './App.css';
-import Section from './components/feedback/Section/Section'
-
+import { useState } from 'react';
+import Section from './components/Section';
+import Container from './components/Container';
+import Statistics from './components/Statistics';
+import Notification from './components/Notification';
+import FeedbackOptions from './components/FeedbackOptions';
 
 export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countFeedback = e => {
+    const buttonName = e.currentTarget.name;
+
+    if (buttonName === 'good') {
+      setGood(prevState => prevState + 1);
+      return;
+    }
+
+    if (buttonName === 'neutral') {
+      setNeutral(prevState => prevState + 1);
+      return;
+    }
+
+    if (buttonName === 'bad') {
+      setBad(prevState => prevState + 1);
+      return;
+    }
+    return;
+
+    // === вариант switch-case ===
+
+    // switch (buttonName) {
+    //   case 'good':
+    //     setGood(prevState => prevState + 1);
+    //     break;
+    //   case 'neutral':
+    //     setNeutral(prevState => prevState + 1);
+    //     break;
+    //   case 'bad':
+    //     setBad(prevState => prevState + 1);
+    //     break;
+    //   default:
+    //     return;
+    // }
+  };
+
+  const getPositiveFeedback = () => {
+    const total = (good * 100) / (neutral + bad + good);
+    return Math.round(total);
+  };
+
+  const options = ['good', 'neutral', 'bad'];
+
   return (
-    <div className="App">
-      <Section title="Leave here ur feedback, please" />
-    </div>
-  )
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={countFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {good + neutral + bad === 0 ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={good + neutral + bad}
+            positivePercentage={getPositiveFeedback()}
+          />
+        )}
+      </Section>
+    </Container>
+  );
 }
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div className={s.options}>
+    {options.map((option, index) => (
+      <button key={index} name={option} onClick={onLeaveFeedback}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
